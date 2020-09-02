c4e9704705f2e914cb28d107fd5332b3a6c8b917
dalengamatheus@gmail.com <dalengamatheus@gmail.com>
2020-09-01 22:02:05 -0300

    Exercicios com IF

---
+++ Exercicios/src/main/Exc_01.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.Scanner;
+
+public class Exc_01 {
+	public static void main(String[] args) {
+		
+		/*1 - Crie um programa para receber um valor qualquer e
+		 *apresente como resposta se esse número é positivo ou 
+		 *negativo.*/
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um número inteiro: ");
+		int num1 = sc.nextInt();
+		
+		if(num1 > 0){
+			System.out.print("O número " + num1 + " é Positivo");
+		}
+		else if (num1 < 0) {
+			System.out.print("O número " + num1 + " é Negativo");
+		}
+		
+		sc.close();
+	}
+}
---
+++ Exercicios/src/main/Exc_02.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.Scanner;
+
+public class Exc_02 {
+	public static void main(String[] args) {
+		
+		/*2 - Crie um programa para receber um valor qualquer
+		 *e apresente como resposta se esse número é par ou 
+		 *impar.*/
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um número inteiro: ");
+		int num1 = sc.nextInt();
+		
+		if(num1 % 2 == 0 ) {
+			System.out.print("O número " + num1 +" é Par.");
+		} else {
+			System.out.print("O número " + num1 +" é Impar.");
+		}
+		
+		
+		sc.close();
+	}
+}
