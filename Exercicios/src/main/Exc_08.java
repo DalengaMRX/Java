93d6b6c8977886a75ca309b1faea62be8181ab7a
dalengamatheus@gmail.com <dalengamatheus@gmail.com>
2020-09-05 23:58:16 -0300

    Calculo de impostos

---
+++ Exercicios/src/main/Exc_08.java
@@ -0,0 +1,38 @@
+package main;
+
+import java.util.Scanner;
+
+public class Exc_08 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		float salario = 0;
+		
+		System.out.print("Insira o valor do seu salário: ");
+		salario = sc.nextFloat();
+		
+		if (salario <= 2000) {
+			System.out.println("Seu salário esta insento de impostos.");
+		}
+		
+		else if(salario <= 3000) {
+			float imposto = (float)(0.08 * 2000);
+			System.out.println("O valor total do seu salário é de R$ "+ (salario - imposto)+ " Reais.");
+		}
+		
+		else if(salario <= 4500) {
+			float imposto1 = (float)(0.18 * 3000);
+			float imposto2 = (float)(0.08 * 1000);
+			System.out.println("O valor total do seu salário é de R$ "+ ((salario - imposto1)- imposto2)+ " Reais.");
+		}
+		
+		else if(salario > 4500) {
+			float imposto1 = (float)(0.28 * 4500);
+			float imposto2 = (float)(0.18 * 1500);
+			float imposto3 = (float)(0.08 * 1000);
+			System.out.println("O valor total do seu salário é de R$ "+ (((salario - imposto1)- imposto2)- imposto3)+ " Reais.");
+		}
+		
+		sc.close();
+	}
+}
