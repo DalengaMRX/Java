1e2b30c87ce62441a2003b7a3dc55281cca6e307
dalengamatheus@gmail.com <dalengamatheus@gmail.com>
2020-09-06 21:58:20 -0300

    Calculo de Fatorial.

---
+++ Exercicios/src/main_for/Exc_05.java
@@ -0,0 +1,31 @@
+/*
+ * author                Matheus Dalenga
+ */
+package main_for;
+
+import java.util.Scanner;
+
+public class Exc_05 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num_1 = 0;
+		int fatorial = 1;
+		
+		System.out.println("*** FATORIAL ***");
+		System.out.println("");
+		
+		System.out.print("Insira um número que vosê deseja saber o seu fatorial: ");
+		num_1 = sc.nextInt();
+		
+	    for (int i = 2; i <= num_1; i++) {
+	    	System.out.println(i);
+	        fatorial = fatorial * i;
+	    }
+	    
+		System.out.println("");
+		System.out.println("O " + num_1 + "! é " + fatorial);
+		
+		sc.close();
+	}
+}
