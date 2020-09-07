b7e438f74a85017644ed1460f1c2ee1d47831825
dalengamatheus@gmail.com <dalengamatheus@gmail.com>
2020-09-06 21:56:01 -0300

    Valores ímpares.

---
+++ Exercicios/src/main_for/Exc_01.java
@@ -0,0 +1,41 @@
+/*
+ * author                Matheus Dalenga
+ */
+package main_for;
+
+import java.util.Scanner;
+
+public class Exc_01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int numero = 0;
+		boolean status = true;
+		
+		System.out.println("*** Intervalos ***");
+		System.out.println("");
+		
+		while(status == true) {
+		
+			System.out.print("Insira um número positivo inteiro: ");
+			numero = sc.nextInt();
+			System.out.println("");
+			
+			if(numero >= 0) {
+			
+				System.out.println("Os números inteiros ímpares de 0 á "+numero+" são: \n");
+				
+				for(int i = 0; i <= numero; i++) {
+					if(i % 2 == 1) {
+						System.out.println(i);
+					}
+				}
+				
+				status = false;
+			} else {
+				System.out.println("*** O número deve ser positivo ! ***\n");
+			}
+		}
+		sc.close();
+	}
+}
