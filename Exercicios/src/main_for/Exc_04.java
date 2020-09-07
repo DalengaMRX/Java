88094f637f371a6360ecb613e2c07149c63d2359
dalengamatheus@gmail.com <dalengamatheus@gmail.com>
2020-09-06 21:57:42 -0300

    Divisão entre 2 números.

---
+++ Exercicios/src/main_for/Exc_04.java
@@ -0,0 +1,53 @@
+/*
+ * author                Matheus Dalenga
+ */
+package main_for;
+
+import java.util.Scanner;
+
+public class Exc_04 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		float num_1 = 0;
+		float num_2 = 0;
+		
+		boolean status = true;
+		
+		String opc;
+		
+		System.out.println("*** DIVISAO ***");
+		System.out.println("");
+		
+		while(status == true) {
+			System.out.print("Insira o primeiro número: ");
+			num_1 = sc.nextFloat();
+			System.out.println("");
+			
+			System.out.print("Insira o segundo número: ");
+			num_2 = sc.nextFloat();
+			System.out.println("");
+			
+			if(num_2 > 0 || num_2 < 0){
+				System.out.print("O resultado da divisão " + num_1 + "/"+ num_2 + " é: "+((float)(num_1/num_2)));
+				System.out.println("\n");
+			}else{
+				System.out.println("Impossível dividir por 0!");
+			}
+			
+			System.out.println("Deseja realizar outra divisão? s / n");
+			opc = sc.next();
+			
+			if(opc.equals("s") || opc.equals("S")) {
+				continue;
+			}else {
+				status = false;
+			}
+			
+		}
+		
+		System.out.println("Fim!!");
+		
+		sc.close();
+	}
+}
